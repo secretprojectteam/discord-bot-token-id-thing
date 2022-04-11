0b03ff4f39998b666bf80a08a2436a4453e736f6
John Pham <johnphammail@gmail.com>
2022-04-10 21:50:14 -0700

    Add responding

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { Client, Intents } = require('discord.js');
 require('dotenv').config();
 const { DISCORD_BOT_KEY, DISCORD_CLIENT_ID } = process.env;
+
+const DISCORD_COMMAND_PREFIX = '!view';
 
 /**
  * The Discord bot needs the following Bot Permissions:
@@ -10,8 +13,22 @@ const { DISCORD_BOT_KEY, DISCORD_CLIENT_ID } = process.env;
  * 5. Attach Files
  */
 const DISCORD_BOT_PERMISSIONS = 274877959168;
-
 const DISCORD_INVITE_LINK = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&permissions=${DISCORD_BOT_PERMISSIONS}&scope=bot
 `;
-
 console.log(`To add this bot to a server, go here: ${DISCORD_INVITE_LINK}`);
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login(DISCORD_BOT_KEY);
+
+client.on('messageCreate', (message) => {
+    if (message.content.startsWith(DISCORD_COMMAND_PREFIX)) {
+        console.log(message);
+        message.reply('hello');
+    }
+});
